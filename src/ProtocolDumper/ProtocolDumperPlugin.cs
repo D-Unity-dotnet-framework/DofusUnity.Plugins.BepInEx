7c74a0048a34c2c5780cd2446ee682bc2432ec58
thenameless314159 <thenameless314159@gmail.com>
2024-08-17 15:30:21 +0200

    Initial solution commit

---
+++ src/ProtocolDumper/ProtocolDumperPlugin.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+
+namespace ProtocolDumper;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class ProtocolDumperPlugin : BasePlugin
+{
+	internal new static ManualLogSource Log = null!;
+
+	public override void Load()
+	{
+		Log = base.Log;
+		Log.LogInfo($"Plugin '{MyPluginInfo.PLUGIN_NAME}' has successfully been loaded!");
+	}
+}
